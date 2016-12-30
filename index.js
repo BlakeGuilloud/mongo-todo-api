81a1ffd90ce1f4af39aba7afdc8dc08466de343d
Blake Guilloud <fbguillo@gmail.com>
2016-12-30 17:32:35 -0500

    Setting up dev env. First route

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const todoRoutes = require('./lib/routes/todo.routes');
+
+const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
+const port = process.env.PORT || 8100;
+
+mongoose.connect(db);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.use('/todos', todoRoutes);
+
+app.listen(port, () => console.log('app listening on port: ', port));
